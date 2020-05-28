9a587f92ae4e0db0c2b63eb83a3834642e346787
Luiz Chaves <luizchaves97@gmail.com>
2020-05-28 02:14:21 -0300

    feat: integrate to api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Text } from 'react-native';
 
 import { MainContainer } from './style';
+import api from '../../services/api';
 
 const Main = () => {
+  const [helloText, setHelloText] = useState('');
+
+  const getHelloWorld = useCallback(async () => {
+    const response = await api.get('/');
+
+    setHelloText(response.data);
+  }, []);
+
+  useEffect(() => {
+    getHelloWorld();
+  }, [getHelloWorld]);
+
   return (
     <MainContainer>
-      <Text>Home Screen</Text>
+      <Text>{helloText.greeting}</Text>
     </MainContainer>
   );
 };
